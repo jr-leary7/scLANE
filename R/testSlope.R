77362cfda757cee37a64668f40d172d3bf5fab24
jr-leary7 <jrleary@live.unc.edu>
2021-11-02 22:38:38 -0400

    ACTUALLY fixed testSlope()

--- R/testSlope.R
+++ R/testSlope.R
@@ -1,22 +1,22 @@
 #' Test whether a gene's expression is increasing or decreasing over a window of pseudotime.
 #'
-#'  @name testSlope
-#'  @description This function tests whether the slope of a gene's \eqn{\hat{\beta}} for pseudotime has a significant effect on expression within a pseudotime window. In short, it tells us wether a gene's expression is changing over an interval or not.
-#'  @importFrom dplyr %>% arrange mutate
-#'  @param model.list A list of \code{marge} models. Defaults to NULL.
-#'  @param expr A matrix or data.frame of raw integer counts. Defaults to NULL.
-#'  @param pt A data.frame of pseudotime values for each cell. Defaults to NULL.
-#'  @param adj.method The method used to adjust the \emph{p}-values for each slope. Defaults to "BH" for Benjamini-Hochberg.
-#'  @param fdr.alpha The FDR statistical significance cutoff used after MHT adjustment. Defaults to 0.05.
-#'  @return A dataframe containing the genes, breakpoints, and slope \emph{p}-values from each model.
-#'  @seealso \code{\link{p.adjust}}
-#'  @export
-#'  @examples
+#' @name testSlope
+#' @description This function tests whether the slope of a gene's \eqn{\beta} for pseudotime has a significant effect on expression within a pseudotime window. In short, it tells us whether a gene's expression is changing over an interval or not.
+#' @importFrom dplyr %>% arrange mutate
+#' @param model.list A list of \code{marge} models. Defaults to NULL.
+#' @param expr A matrix or data.frame of raw integer counts. Defaults to NULL.
+#' @param pt A data.frame of pseudotime values for each cell. Defaults to NULL.
+#' @param adj.method The method used to adjust the \emph{p}-values for each slope. Defaults to "BH" for Benjamini-Hochberg.
+#' @param fdr.alpha The FDR statistical significance cutoff used after MHT adjustment. Defaults to 0.05.
+#' @return A dataframe containing the genes, breakpoints, and slope \emph{p}-values from each model.
+#' @seealso \code{\link{p.adjust}}
+#' @export
+#' @examples
 #'  testSlope(model.list = marge_list, fdr.alpha = 0.01)
 
 testSlope <- function(model.list = NULL, adj.method = "BH") {
   # check inputs
-  if (is.null(model.list)) { stop("You forgot to provide a model list to testChange().") }
+  if (is.null(model.list)) { stop("You forgot to provide a model list to testSlope().") }
   if (!all(as.logical(lapply(model.list, function(x) "glm" %in% class(x))))) { stop("All models must be of class glm.") }
   # loops on loops
   genes <- names(model.list)

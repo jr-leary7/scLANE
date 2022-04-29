1c4996878a8de44eae35844f7e0ccd11a0516f46
jr-leary7 <jrleary@live.unc.edu>
2022-04-29 16:26:41 -0400

    switched scoring of null GEE model to use RcppEigen matrix multiplication

---
+++ R/scLANE-package.R
@@ -0,0 +1,14 @@
+## usethis namespace: start
+#' @useDynLib scLANE, .registration = TRUE
+## usethis namespace: end
+NULL
+
+## usethis namespace: start
+#' @importFrom Rcpp sourceCpp
+## usethis namespace: end
+NULL
+
+## usethis namespace: start
+#' @import RcppEigen
+## usethis namespace: end
+NULL

bc2d893978113fb37724c166acc4dfc95bfd5fa4
jr-leary7 <jrleary@live.unc.edu>
2022-12-07 16:26:50 -0500

    Fixing getResultsDE() bugs AGAIN

--- R/GetResultsDE.R
+++ R/GetResultsDE.R
@@ -5,7 +5,8 @@
 #' @description This function turns the nested list differential expression results of \code{\link{testDynamic}} and turns them into a tidy \code{data.frame}.
 #' @import magrittr
 #' @importFrom purrr map_dfr
-#' @importFrom dplyr arrange mutate case_when with_groups relocate
+#' @importFrom dplyr arrange mutate across case_when with_groups relocate
+#' @importFrom tidyselect everything
 #' @importFrom stats p.adjust p.adjust.methods
 #' @param test.dyn.results The nested list returned by \code{\link{testDynamic}}. Defaults to NULL.
 #' @param p.adj.method (Optional) The method used to adjust \emph{p}-values for multiple hypothesis testing. Defaults to "bonferroni".
@@ -28,11 +29,18 @@ getResultsDE <- function(test.dyn.results = NULL,
   # check inputs
   if (is.null(test.dyn.results)) { stop("Please provide a result list.") }
   if (!p.adj.method %in% stats::p.adjust.methods) { stop("Please choose a valid p-value adjustment method.") }
-  # take non-dataframe items of nested list structure from testDynamic() and turn them into a dataframe
-  result_df <- purrr::map_dfr(test.dyn.results, \(x) as.data.frame(rbind(x[[1]][c(1:12)]))) %>%
+  # iterates first over genes, then over lineages per-gene & coerces to final data.frame after unlisting everything
+  result_df <- purrr::map_dfr(test.dyn.results,
+                              function(x) {
+                                purrr::map_dfr(x,
+                                               function(y){
+                                                 as.data.frame(rbind(y[c(1:12)])) %>%
+                                                   dplyr::mutate(dplyr::across(tidyselect::everything(), \(z) unname(unlist(z))))
+                                               })
+                              }) %>%
                dplyr::arrange(P_Val) %>%
                dplyr::mutate(P_Val_Adj = stats::p.adjust(P_Val, method = p.adj.method),
-                             Gene_Dynamic_Lineage = dplyr::if_else(P_Val_Adj < fdr.cutoff, 1, 0)) %>%
+                             Gene_Dynamic_Lineage = dplyr::case_when(P_Val_Adj < fdr.cutoff ~ 1, TRUE ~ 0)) %>%
                dplyr::with_groups(Gene,
                                   dplyr::mutate,
                                   Gene_Dynamic_Overall = max(Gene_Dynamic_Lineage)) %>%

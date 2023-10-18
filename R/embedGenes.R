b38cdf25c58767b409ae5956b4dd4c0cf641c6c5
Jack Leary <j.leary@ufl.edu>
2023-10-18 12:08:03 -0400

    improved gene embedding and smoothed counts functions

--- R/embedGenes.R
+++ R/embedGenes.R
@@ -1,4 +1,4 @@
-#' Generate a table of fitted values and celltype metadata for genes of interest.
+#' Generate PCA & UMAP embeddings of fitted gene dynamics.
 #'
 #' @name embedGenes
 #' @author Jack Leary
@@ -9,8 +9,9 @@
 #' @description Embed genes in dimension-reduced space given a smoothed counts matrix.
 #' @param smoothed.counts The output from \code{\link{smoothedCountsMatrix}}. Defaults to NULL.
 #' @param genes A character vector of genes to embed. If not specified, all genes in \code{smoothed.counts} are used. Defaults to NULL.
+#' @param pca.init A boolean specifying whether or not the embedded PCs should be used as initialization for clustering and UMAP. The default is to cluster / embed the raw dynamics i.e., defaults to FALSE.
 #' @param pc.embed (Optional) How many PCs should be used to cluster the genes and run UMAP? Defaults to 30.
-#' @param pcs.return (Optional) How many principal components should be included in the output? Defaults to 2.
+#' @param pc.return (Optional) How many principal components should be included in the output? Defaults to 2.
 #' @param cluster.genes (Optional) Should genes be clustered in PCA space using the Leiden algorithm? Defaults to TRUE.
 #' @param gene.meta.data (Optional) A data.frame of metadata values for each gene (HVG status, Ensembl ID, gene biotype, etc.) that will be included in the result table. Defaults to NULL.
 #' @param k.param (Optional) The value of nearest-neighbors used in creating the SNN graph prior to clustering & in running UMAP. Defaults to 20.
@@ -27,8 +28,9 @@
 
 embedGenes <- function(smoothed.counts = NULL,
                        genes = NULL,
+                       pca.init = FALSE,
                        pc.embed = 30,
-                       pcs.return = 2,
+                       pc.return = 2,
                        cluster.genes = TRUE,
                        gene.meta.data = NULL,
                        k.param = 20,
@@ -44,20 +46,41 @@ embedGenes <- function(smoothed.counts = NULL,
                                              n = pc.embed,
                                              center = TRUE,
                                              scale. = TRUE)
-  smoothed_counts_umap <- uwot::umap(smoothed_counts_pca$x,
-                                     n_components = 2,
-                                     metric = "cosine",
-                                     n_neighbors = k.param,
-                                     init = "spectral")
+  if (pca.init) {
+    smoothed_counts_umap <- uwot::umap(smoothed_counts_pca$x,
+                                       n_components = 2,
+                                       metric = "cosine",
+                                       n_neighbors = k.param,
+                                       init = "spectral",
+                                       nn_method = "annoy")
+  } else {
+    smoothed_counts_umap <- uwot::umap(smoothed.counts,
+                                       n_components = 2,
+                                       metric = "cosine",
+                                       n_neighbors = k.param,
+                                       init = "spectral",
+                                       nn_method = "annoy")
+  }
   # clustering w/ silhouette score parameter tuning
   if (cluster.genes) {
-    smoothed_counts_snn <- bluster::makeSNNGraph(smoothed_counts_pca$x,
-                                                 k = k.param,
-                                                 type = "jaccard",
-                                                 BNPARAM = BiocNeighbors::AnnoyParam(distance = "Cosine"))
+    if (pca.init) {
+      smoothed_counts_snn <- bluster::makeSNNGraph(smoothed_counts_pca$x,
+                                                   k = k.param,
+                                                   type = "jaccard",
+                                                   BNPARAM = BiocNeighbors::AnnoyParam(distance = "Cosine"))
+    } else {
+      smoothed_counts_snn <- bluster::makeSNNGraph(smoothed.counts,
+                                                   k = k.param,
+                                                   type = "jaccard",
+                                                   BNPARAM = BiocNeighbors::AnnoyParam(distance = "Cosine"))
+    }
     if (is.null(resolution.param)) {
-      dist_matrix <- stats::as.dist(1 - coop::tcosine(x = smoothed_counts_pca$x))
-      clust_runs <- purrr::map(c(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7), \(r) {
+      if (pca.init) {
+        dist_matrix <- stats::as.dist(1 - coop::tcosine(x = smoothed_counts_pca$x))
+      } else {
+        dist_matrix <- stats::as.dist(1 - coop::tcosine(x = smoothed.counts))
+      }
+      clust_runs <- purrr::map(seq(0.1, 0.7, by = 0.1), \(r) {
         smoothed_counts_clust <- igraph::cluster_leiden(smoothed_counts_snn,
                                                         objective_function = "modularity",
                                                         resolution_parameter = r)
@@ -83,8 +106,8 @@ embedGenes <- function(smoothed.counts = NULL,
     cluster_vec <- NA_integer_
   }
   # prepare results
-  pca_df <- as.data.frame(smoothed_counts_pca$x[, seq(pcs.return)])
-  colnames(pca_df) <- paste0("pc", seq(pcs.return))
+  pca_df <- as.data.frame(smoothed_counts_pca$x[, seq(pc.return)])
+  colnames(pca_df) <- paste0("pc", seq(pc.return))
   gene_df <- data.frame(gene = genes,
                         leiden = cluster_vec,
                         umap1 = smoothed_counts_umap[, 1],

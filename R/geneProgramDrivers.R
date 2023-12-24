2e0eec580e5e8ca9bffc70e8cd91aa931c170bda
Jack Leary <j.leary@ufl.edu>
2023-12-24 03:40:51 -0500

    added parallel processing to geneProgramDrivers

--- R/geneProgramDrivers.R
+++ R/geneProgramDrivers.R
@@ -4,6 +4,11 @@
 #' @author Jack Leary
 #' @importFrom Matrix Matrix
 #' @importFrom purrr map reduce
+#' @importFrom foreach foreach registerDoSEQ
+#' @importFrom parallel makeCluster clusterEvalQ stopCluster
+#' @importFrom withr with_output_sink
+#' @importFrom utils txtProgressBar setTxtProgressBar
+#' @importFrom doSNOW registerDoSNOW
 #' @importFrom stats cor.test p.adjust
 #' @importFrom dplyr arrange desc mutate filter
 #' @description This function computes the correlation
@@ -13,6 +18,8 @@
 #' @param cor.method (Optional) The correlation method to be used. Defaults to "spearman".
 #' @param fdr.cutoff (Optional) The FDR threshold for determining statistical significance. Defaults to 0.01.
 #' @param p.adj.method (Optional) The method used to adjust \emph{p}-values for multiple hypothesis testing. Defaults to "holm".
+#' @param n.cores (Optional) The number of cores used when iterating over genes to perform testing. Defaults to 2.
+#' @param verbose (Optional) Should a progress bar be printed to the console during processing? Defaults to TRUE.
 #' @return Either a \code{Seurat} or \code{SingleCellExperiment} object if \code{expr.mat} is in either form, or a data.frame containing per-cell program scores if \code{expr.mat} is a matrix.
 #' @seealso \code{\link{geneProgramScoring}}
 #' @seealso \code{\link[stats]{cor.test}}
@@ -39,7 +46,9 @@ geneProgramDrivers <- function(expr.mat = NULL,
                                gene.program = NULL,
                                cor.method = "spearman",
                                fdr.cutoff = 0.01,
-                               p.adj.method = "holm") {
+                               p.adj.method = "holm",
+                               n.cores = 2L,
+                               verbose = TRUE) {
   # check inputs
   if (is.null(expr.mat) || is.null(genes) || is.null(gene.program)) { stop("Arguments to geneProgramDrivers() are missing.") }
   cor.method <- tolower(cor.method)
@@ -54,16 +63,50 @@ geneProgramDrivers <- function(expr.mat = NULL,
   } else if (inherits(expr.mat, "dgCMatrix")) {
     counts_matrix <- Matrix::Matrix(expr.mat, sparse = FALSE)
   }
+  # set up parallel processing
+  if (n.cores > 1L) {
+    cl <- parallel::makeCluster(n.cores)
+    doSNOW::registerDoSNOW(cl)
+  } else {
+    cl <- foreach::registerDoSEQ()
+  }
+  # set up progress bar
+  if (verbose) {
+    withr::with_output_sink(tempfile(), {
+      pb <- utils::txtProgressBar(0, length(genes), style = 3)
+    })
+    progress_fun <- function(n) utils::setTxtProgressBar(pb, n)
+    snow_opts <- list(progress = progress_fun)
+  } else {
+    snow_opts <- list()
+  }
   # iteratively compute correlations & p-values
-  cor_tests <- purrr::map(genes, \(g) {
-    cor_res <- stats::cor.test(counts_matrix[g, ],
+  cor_tests <- foreach::foreach(g = seq(genes),
+                                .combine = "list",
+                                .multicombine = ifelse(length(genes) > 1, TRUE, FALSE),
+                                .maxcombine = ifelse(length(genes) > 1, length(genes), 2),
+                                .packages = c("stats"),
+                                .errorhandling = "pass",
+                                .inorder = TRUE,
+                                .verbose = FALSE,
+                                .options.snow = snow_opts) %dopar% {
+    cor_res <- stats::cor.test(counts_matrix[genes[g], ],
                                gene.program,
                                method = cor.method,
                                exact = FALSE)
-    cor_df <- data.frame(gene = g,
+    cor_df <- data.frame(gene = genes[g],
                          corr = unname(cor_res$estimate),
                          pvalue = cor_res$p.value)
-    return(cor_df)
+    cor_df
+  }
+  # end parallelization & clean up each worker node
+  withr::with_output_sink(tempfile(), {
+    if (n.cores > 1L) {
+      parallel::clusterEvalQ(cl, expr = {
+        gc(verbose = FALSE, full = TRUE)
+      })
+      parallel::stopCluster(cl)
+    }
   })
   cor_tests <- purrr::reduce(cor_tests, rbind)
   rownames(cor_tests) <- genes
